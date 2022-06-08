bc006340d0b9baa8563dfd823de4d945efc7095b
Psenkov <leroygorn@gmail.com>
2022-06-08 18:19:24 +0300

    Added models for music app and tests for models

--- src/music/models.py
+++ src/music/models.py
@@ -1,3 +1,57 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class ForumPosted(models.Model):
+    category = models.ForeignKey(to="music.ForumCategory", related_name="posts", on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), related_name="customer", on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4, db_index=True, unique=True)
+    image = models.ImageField(default="default.png", upload_to="media/covers")
+    title = models.CharField(max_length=128, blank=True)
+    description = models.CharField(max_length=2048, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def messages_count(self):
+        return self.thread.count()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class ForumCategory(models.Model):
+    name = models.CharField(max_length=128)
+    uuid = models.UUIDField(default=uuid.uuid4, db_index=True, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ForumComments(models.Model):
+    author = models.ForeignKey(get_user_model(), related_name="users", on_delete=models.CASCADE)
+    messages = models.ForeignKey(to="music.ForumPosted", related_name="thread", on_delete=models.CASCADE)
+    text = models.CharField(max_length=2048, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.text})"
+
+
+class ForumReplies(models.Model):
+    writer = models.ForeignKey(get_user_model(), related_name="commentator", on_delete=models.CASCADE)
+    replies = models.ForeignKey(to="music.ForumComments", related_name="comments", on_delete=models.CASCADE)
+    text = models.CharField(max_length=2048, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.text})"
--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,6 +1,13 @@
+import random
 import unittest
+from datetime import date
 
-from hypothesis import given, strategies as st
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from hypothesis import given
+from hypothesis import strategies as st
+
+from music.models import ForumCategory, ForumComments, ForumPosted
 
 
 class Fibonacci:
@@ -48,29 +55,84 @@ class TestFibonacci(unittest.TestCase):
         self.assertEqual(self.fibonacci.__call__(-1), 'Positive integer number expected, got "-1"')
 
 
-def formatted_name(first_name, last_name, middle_name=''):
+def formatted_name(first_name, last_name, middle_name=""):
     if len(middle_name) > 0:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
+        full_name = first_name + " " + middle_name + " " + last_name
     else:
-        full_name = first_name + ' ' + last_name
+        full_name = first_name + " " + last_name
     return full_name.title()
 
 
 class TestFormattedName(unittest.TestCase):
     def test_name(self):
-        self.assertEqual(formatted_name('Michael', 'Gorn', middle_name=''), 'Michael Gorn')
-        self.assertEqual(formatted_name('Michael', 'Gorn', middle_name='Jordan'), 'Michael Jordan Gorn')
-        self.assertEqual(formatted_name('Michael', 'Gorn'), 'Michael Gorn')
-        self.assertEqual(formatted_name('Michael', ''), 'Michael ')
-        self.assertEqual(formatted_name('', 'Michael', 'Jordan'), ' Jordan Michael')
-        self.assertEqual(formatted_name('', 'Michael'), ' Michael')
+        self.assertEqual(formatted_name("Michael", "Gorn", middle_name=""), "Michael Gorn")
+        self.assertEqual(formatted_name("Michael", "Gorn", middle_name="Jordan"), "Michael Jordan Gorn")
+        self.assertEqual(formatted_name("Michael", "Gorn"), "Michael Gorn")
+        self.assertEqual(formatted_name("Michael", ""), "Michael ")
+        self.assertEqual(formatted_name("", "Michael", "Jordan"), " Jordan Michael")
+        self.assertEqual(formatted_name("", "Michael"), " Michael")
 
     @unittest.expectedFailure
     def test_exception(self):
         self.assertEqual(
-            formatted_name('Michael'), 'TypeError: formatted_name() missing 1 required positional argument: "last_name"'
+            formatted_name("Michael"),
+            'TypeError: formatted_name() missing 1 required positional argument: "last_name"',
         )
 
 
-if __name__ == '__main__':
-    unittest.main()
+User = get_user_model()
+user_instance = User.objects.create(username=random.randint(1, 10000), email="email", password="password")
+
+
+def sample_forum_posted(title, **params):
+    defaults = {
+        "description": "Some text",
+        "category": ForumCategory.objects.create(name="Misha"),
+        "user": user_instance,
+    }
+    defaults.update(params)
+    return ForumPosted.objects.create(title=title, **defaults)
+
+
+def sample_forum_comments(**params):
+    defaults = {"text": "text", "author": user_instance, "messages": sample_forum_posted(title="Test")}
+    defaults.update(params)
+    return ForumComments.objects.create(**defaults)
+
+
+class TestForumModel(unittest.TestCase):
+    def setUp(self) -> None:
+        self.category = ForumCategory.objects.create(name="Misha")
+        self.messages_count = 1
+        self.test_forum_posted = sample_forum_posted(title="test_mytest")
+        self.date = sample_forum_comments(
+            text="test_text", messages=sample_forum_posted(title="Test"), date=date(2022, 6, 8)
+        )
+        for i in range(self.messages_count):
+            sample_forum_comments(messages=self.test_forum_posted, text="test_text")
+
+    def tearDown(self) -> None:
+        self.test_forum_posted.delete()
+
+    def test_messages_count_normal_case(self):
+        self.assertEqual(self.messages_count, self.test_forum_posted.messages_count())
+
+    def test_title_limit(self):
+        with self.assertRaises(ValidationError):
+            sample_forum_posted(title="i" * 2000)
+
+    def test_description_limit(self):
+        with self.assertRaises(ValidationError):
+            sample_forum_comments(text="i" * 20000)
+
+    def test_category_name(self):
+        self.assertEqual(str(self.category), "Misha")
+
+    def test_date_field(self):
+        with self.assertRaises(ValueError):
+            self.assertEqual(
+                self.date,
+                sample_forum_comments(
+                    text="test_text", messages=sample_forum_posted(title="Test"), date=date(6, 2022, 8)
+                ),
+            )
