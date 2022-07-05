971d5329bb3b47ceea4b24cb3f5a7c36ad748227
LeroyGorn <leroygorn@gmail.com>
2022-07-06 02:09:36 +0300

    Multiprocessor tasks

--- commands/start_server_main.sh
+++ commands/start_server_main.sh
@@ -3,4 +3,5 @@
 echo "Hello from mushub docker"
 python src/manage.py migrate --settings=config.settings.${MODE}
 python src/manage.py collectstatic --noinput --settings=config.settings.${MODE}
-python src/manage.py runserver --settings=config.settings.${MODE} 0:${WSGI_PORT}
+
+gunicorn -w ${WSGI_WORKERS} -b 0:${WSGI_PORT} --chdir ./src config.wsgi:application --log-level=${WSGI_LOG_LEVEL}
